d19b99a7af2411687623e3ba110aea3a17016aa5
echewisi <josephechewisi@gmail.com>
2023-06-13 00:16:10 +0100

    footer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Route, Switch, Router, BrowserRouter } from 'react-router-dom';
+import { Route, Router, Routes } from 'react-router-dom';
 import App from './App';
 import Header from './components/header';
 import Footer from './components/footer';
@@ -10,9 +10,9 @@ const routing=(
   <Router>
     <React.StrictMode>
     <Header />
-      <Switch>
+      <Routes>
         <Route exact path= "/" component= {App} />
-      </Switch>
+      </Routes>
     <Footer />
     </React.StrictMode>
   </Router>
