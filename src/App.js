35a471aaede864b06730c90c6529c53edaa345d7
echewisi <josephechewisi@gmail.com>
2023-07-10 23:05:55 +0100

    app mod

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import Posts from './components/Posts'
+import PostLoadingComponent from './components/PostLoading';
+function App(){
+  const PostLoading= PostLoadingComponent(Posts);
+  const [appState, setAppState]= useState({
+    loading: false,
+    posts: true,
+  });
+  useEffect(()=>{
+    setAppState({loading: true});
+    const apiUrl= 'http://127.0.0.1:8000/api/';
+    fetch(apiUrl)
+    .then((data)=> data.json())
+    .then((posts)=> {
+        setAppState({loading: false, posts:posts});
+    });
+  }, [setAppState]);
+  return(
+    <div className='App'>
+      <h1>Latest Posts</h1>
+      <PostLoading Isloading={appState.loading} posts={appState.posts} />
     </div>
-  );
+  )
 }
+
+
+
+
 
 export default App;
